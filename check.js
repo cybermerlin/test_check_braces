b95669f4deffe6347fa135bb728a4e0a6c3e5775
cybermerlin <cybermerlin@users.noreply.github.com>
2020-05-15 13:08:20 +0300

    add recursion version for to parallelize and O^n complexity

--- check.js
+++ check.js
@@ -1,3 +1,73 @@
+/**
+ * with recursion function, with one run. Can be parallelized
+ */
+function isValid(str) {
+  const braces = {
+    '(': ')',
+    '{': '}',
+    '[': ']'
+  };
+  let result = true;
+
+  /**
+   * find and check
+   * @param str 
+   * @param brace 
+   */
+  function recCheck(str, brace) {
+      let r = {valid: false, position: -1};
+
+      for (let i=0; i< str.length; i++) {
+          if (str[i] === brace) {
+            r.position = i;
+            r.valid = true;
+            break;
+          }
+          else if (Object.values(braces).includes(str[i])) {
+            r.position = i;
+            r.valid = false;
+            break;
+          }
+
+          switch (str[i]) {
+              case '[':
+              case '{':
+              case '(':
+                  let rTemp = recCheck(str.substr(i+1), braces[str[i]]);
+                  if (rTemp.position === -1 || !rTemp.valid) return r;
+                  else i += rTemp.position + 1;
+                break;
+          }
+      }
+
+      return r;
+  }
+
+
+  for (let i=0; i< str.length; i++) {
+    if (!result) break;
+    
+    switch (str[i]) {
+        case '(':
+        case '[':
+        case '{':
+            result = false;
+            let res = recCheck(str.substr(i+1), braces[str[i]]);
+            if (res.position === -1) res.valid = false;
+            else i += res.position + 1;
+            result = res.valid;
+            break;
+    }
+  }
+  
+  return result;
+}
+
+
+
+
+// ------------------- simplest tries ------------------
+
 function check(s) {
 	var c = 0,
 		q = 0,
